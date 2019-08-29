c6a46094ba2573dc7a6334822f3ac5f2b5386c1e
lakshay1221 <54670318+lakshay1221@users.noreply.github.com>
2019-08-29 17:32:00 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+    cout<<"enter your age";
+    cin>>age;
+    if(age<35)
+        cout<<"Your age is median";
+    else
+        cout<<"you are old";
+}
