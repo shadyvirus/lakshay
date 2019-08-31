013893f7680163ddc406d3853cc7cf25e06cfa0b
lakshay1221 <54670318+lakshay1221@users.noreply.github.com>
2019-08-31 17:04:50 +0530

    Add files via upload

---
+++ functions.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int add(int x,int y)
+{
+    int z;
+    z=x+y;
+    return z;
+}
+int main()
+{
+    int a,b,c;
+    cout<<"Enter the numbers you want to add:";
+    cin>>a;
+    cin>>b;
+    c=add(a,b);
+    cout<<"your number is:"<<c;
+    return 0;
+}
---
+++ prime.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int n,count=0;
+cout<<"Enter a no. ";
+cin>>n;
+for(int i=2;i<=n;i++)
+    {
+        if(n%i==0)
+        count++;
+    }
+if(count==1)
+cout<<"It's a Prime\n";
+else
+cout<<"It's not a Prime\n";
+return 0;
+}
